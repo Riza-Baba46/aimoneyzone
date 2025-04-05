d3dd7990fb76b0c0b84fd955da821df220bb3e37
Riza Baba <rizahakankayiran@yandex.com>
2025-04-05 21:18:36 +0300

    footer ads eklendi

--- js/ads.js
+++ js/ads.js
@@ -1,17 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const headerAd = document.getElementById("ads-header");
-    if (headerAd) {
-      headerAd.innerHTML = `
-        <ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-5535291444295129"
-             data-ad-slot="3561744551"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-        <script>
-             (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-      `;
-    }
-  });
-  
+  // Header Reklamı
+  const headerAd = document.getElementById("ads-header");
+  if (headerAd) {
+    headerAd.innerHTML = `
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-5535291444295129"
+           data-ad-slot="3561744551"
+           data-ad-format="auto"
+           data-full-width-responsive="true"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+    `;
+  }
+
+  // Middle (In-Article) Reklamı
+  const middleAd = document.getElementById("ads-middle");
+  if (middleAd) {
+    middleAd.innerHTML = `
+      <ins class="adsbygoogle"
+           style="display:block; text-align:center;"
+           data-ad-layout="in-article"
+           data-ad-format="fluid"
+           data-ad-client="ca-pub-5535291444295129"
+           data-ad-slot="7235000917"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+    `;
+  }
+
+  // Footer (Multiplex) Reklamı
+  const footerAd = document.getElementById("ads-footer");
+  if (footerAd) {
+    footerAd.innerHTML = `
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-format="autorelaxed"
+           data-ad-client="ca-pub-5535291444295129"
+           data-ad-slot="3894521701"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+    `;
+  }
+});
